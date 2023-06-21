b4ecd90bd8b741faa0d02fdc07b3bea1a4c0de6e
GGGCI <h104857621@gmail.com>
2023-06-21 10:59:39 +0800

    disabled

--- TTC.js
+++ TTC.js
@@ -8,9 +8,50 @@ const ip = "127.0.0.1";
 const express = require('express');
 const multer = require('multer');
 const app = express();
+const bodyParser = require('body-parser');
 
 const { exec, spawn } = require("child_process");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/your-endpoint', (req, res) => {
+  const selectedValue = req.body.value;
+
+  // 在这里处理ComboBox的值
+  console.log(selectedValue);
+
+  // 做其他操作...
+
+  // 返回响应给前端
+  res.send('Received the value: ' + selectedValue);
+});
+/*
+app.get('/', (req, res) => {
+  // 根据需要确定是否禁用<select>元素
+  const isDisabled_CC = true;
+  const isDisabled_key = true;
+  const isDisabled_cts = true;
+  const isDisabled_encData = true;
+  // 渲染模板，并传递禁用状态给模板
+  res.render('TTC', { isDisabled_CC ,isDisabled_cts,isDisabled_encData,isDisabled_key});
+});
+app.post('/your-endpoint', (req, res) => {
+  const selectOption = req.body.value;
+  console.log(selectOption);
+  app.get('/', (req, res) => {
+  const isDisabled_CC = true;
+  const isDisabled_key = true;
+  const isDisabled_cts = true;
+  const isDisabled_encData = true;
+  if(selectOption == "query")
+  {
+    isDisabled_CC = false;
+    isDisabled_encData = false;
+  }
+  // 渲染模板，并传递禁用状态给模板
+  res.render('TTC', { isDisabled_CC ,isDisabled_cts,isDisabled_encData,isDisabled_key});
+  });
+  
+});*/
 const storage = multer.diskStorage({
   destination:function(req,file,cb){
     cb(null,'uploads/')
@@ -23,12 +64,12 @@ const upload = multer({
   storage: storage,
  });
 //app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('html'));
+app.use(express.static('views'));
 app.get('', (req, res) =>{
-  res.sendFile(path.join(__dirname,'html','TTC.html'));
+  res.sendFile(path.join(__dirname,'views','TTC.html'));
 });
 
-app.post('/uploads', upload.fields([{name:'key'},{name:"CC"},{name:"cts"}]), (req, res) => {
+app.post('/uploads', upload.fields([{name:'key'},{name:"CC"},{name:"cts"},{name:"encData"}]), (req, res) => {
   // 文件上传后的处理逻辑
   try{
     const uploadedFiles = req.files.CC;
@@ -48,7 +89,7 @@ app.post('/uploads', upload.fields([{name:'key'},{name:"CC"},{name:"cts"}]), (re
     console.error(error);
   }
   console.log("prepare to query");
-  exec("cd uploads/ && LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/llvm-14/lib/ ./server -q",(error,stdout,stderr)=>{
+  exec("cd uploads/ && ./server -e",(error,stdout,stderr)=>{
     if(error){
       console.error(`error: ${error}`);
     return;
